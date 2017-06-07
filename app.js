0a9706cebd5975bbcd0326f35a3a51f779ccee67
Anthony Shalagin <anthony.shalagin@gmail.com>
2017-06-07 13:55:32 -0400

    Format JSON objects

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=2180%20East%2027%20Street,%20Brooklyn,%20NY',
     json: true
 }, (error,response,body) => { //callback function gets called once the data comes back from the HTTP endpoint
-    console.log(body);
+    console.log(JSON.stringify(body, undefined, 2)); //third paramter is how many spaces you want 
 });

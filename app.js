e561191a6bed777aac2f2df52834f2a8eaaec1d4
Anthony Shalagin <anthony.shalagin@gmail.com>
2017-06-07 17:22:07 -0400

    Added encoding functionality

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ const argv = yargs //object that stores the final parsed output
     .alias('help', 'h')
     .argv;
 
-console.log(argv);
+var encodedAddress = encodeURIComponent(argv.a); //Encode the address to pass it into the url with the correct format
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=2180%20East%2027%20Street,%20Brooklyn,%20NY',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 }, (error,response,body) => { //callback function gets called once the data comes back from the HTTP endpoint
     console.log(`Address: ${body.results[0].formatted_address}`);

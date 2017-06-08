1e91e9c1cf8ad88366e8d100ee3d5881ac9778f6
Anthony Shalagin <anthony.shalagin@gmail.com>
2017-06-08 15:51:01 -0400

    Added basic error handling for invalid addresses

--- app.js
+++ app.js
@@ -20,6 +20,14 @@ request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 }, (error,response,body) => { //callback function gets called once the data comes back from the HTTP endpoint
+
+    if (error) {
+        console.log('Unable to connect to google servers');
+    }
+    else if (body.status === 'ZERO_RESULTS') {
+        console.log('Unable to find that address');
+    }
+
     console.log(`Address: ${body.results[0].formatted_address}`);
     console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
     console.log(`Longitude: ${body.results[0].geometry.location.lng}`);

e2e265dc9faebf67407f93aa1bc16949d569070f
Anthony Shalagin <anthony.shalagin@gmail.com>
2017-06-15 08:57:18 -0400

    Error handling

--- app-promise.js
+++ app-promise.js
@@ -19,5 +19,15 @@ var encodedAddress = encodeURIComponent(argv.address);
 var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
 
 axios.get(geocodeUrl).then((response) => { //get returns a promise
+    if (response.data.status === 'ZERO_RESULTS') {
+        throw new Error('Unable to find that address');
+    }
     console.log(response.data);
+}).catch((e) => {
+    if (e.code === 'ENOTFOUND') {
+        console.log('Unable to connect to API servers');
+    } else {
+        console.log(e.message);
+    }
+
 });
